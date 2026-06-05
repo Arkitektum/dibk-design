2a6d4fa968fc8d4bc4f5122e7491b9fdd1483491
Benjamin Dehli <benjamin@arkitektum.no>
2026-06-05 16:11:50 +0200

    Remove unnecessary @ts-expect-error for SVG URL import
    The TypeScript configuration or type inference has been updated,
    meaning importing SVG files with the `?url` suffix no longer
    causes a type error. This allows for the removal of the suppression
    comment, cleaning up the code.

--- .storybook/dibkTheme.ts
+++ .storybook/dibkTheme.ts
@@ -1,6 +1,4 @@
 import { create } from 'storybook/theming';
-// @ts-expect-error SVGs can be imported as ReactComponent or URL depending on setup
-
 import dibkLogoWithTitle from '../src/assets/svg/dibk-logo-with-title.svg?url';
 
 const dibkTheme = create({
